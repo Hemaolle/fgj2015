723e0ce42c039c80633cdcdee64549d49ad5b7c4
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-01-23 22:59:30 +0200

    Change to player movement
    -When the players thrust at the same time, the ship starts to move
    forward immediately and the angular velocity stops with some speed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public float forceStrength = 1;
     public float thrusterDistanceFromCenter = 1;
+    public float velocityDecelerationWhenBothPress = 1;
 
     // Use this for initialization
     void Start()
@@ -18,6 +19,21 @@ public class PlayerMovement : MonoBehaviour
         Vector3 worldForcePosition;
         // Rotate based on object rotation.
         Vector3 thrustForce = transform.rotation * Vector3.up * forceStrength;
+
+        if (Input.GetButton("Player1Thrust") && Input.GetButton("Player2Thrust"))
+        {
+            Vector3 newAngularVelocity = rigidbody.angularVelocity;
+            if (Mathf.Abs(rigidbody.angularVelocity.z) > velocityDecelerationWhenBothPress * Time.deltaTime) {
+                newAngularVelocity.z = rigidbody.angularVelocity.z + -Mathf.Sign(rigidbody.angularVelocity.z) * velocityDecelerationWhenBothPress * Time.deltaTime;
+            }
+            else {
+                newAngularVelocity.z = 0;
+            }
+            rigidbody.angularVelocity = newAngularVelocity;
+            Debug.Log(newAngularVelocity.z);
+            float velocityMagnitude = rigidbody.velocity.magnitude;
+            rigidbody.velocity = transform.rotation * Vector3.up * velocityMagnitude;
+        }
 
         if (Input.GetButton("Player1Thrust"))
         {
