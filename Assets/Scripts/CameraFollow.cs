b7c4d82761a341e1313f192e4525627e0479ac65
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-01-23 22:11:59 +0200

    Basic player movement.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform objectToFollow;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(objectToFollow.position.x, objectToFollow.position.y, transform.position.z);
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
+    public float forceStrength = 1;
+    public float thrusterDistanceFromCenter = 1;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+    
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 worldForcePosition;
+        // Rotate based on object rotation.
+        Vector3 thrustForce = transform.rotation * Vector3.up * forceStrength;
+
+        if (Input.GetButton("Player1Thrust"))
+        {
+            worldForcePosition = transform.TransformPoint(Vector3.right * thrusterDistanceFromCenter);
+            rigidbody.AddForceAtPosition(thrustForce, worldForcePosition);
+
+        }
+        if (Input.GetButton("Player2Thrust"))
+        {
+            worldForcePosition = transform.TransformPoint(-Vector3.right * thrusterDistanceFromCenter);
+            rigidbody.AddForceAtPosition(thrustForce, worldForcePosition);
+        }
+    }
 }
