c53d4a7dd5f78f9d3fdc22b2cedff8d47040cdcb
sheepbeo <lequoccuong001@yahoo.com>
2015-01-24 10:57:18 +0200

    basic health bar change physic setting

---
+++ Assets/Scripts/UI/PlayerHealth.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public Slider healthSlider;
+	public Health health;
+
+	void Start() {
+		healthSlider.minValue = 0;
+		healthSlider.maxValue = health.maxHealth;
+	}
+
+	void Update() {
+		healthSlider.value = health.CurrentHealth;
+	}
+}
