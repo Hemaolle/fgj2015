60e63abb8d7cb5e792999f2de7494ce33379cca7
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-01-24 10:56:56 +0200

    Added thruster effects, made a prefab for the player, some drag for the player
    -Also created a HandleThrusting method in PlayerMovement
    -Removed the code that was practically just doing angular drag for the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     public float thrustStrength = 1;
     public float thrusterDistanceFromCenter = 1;
     public float velocityDecelerationWhenBothPress = 1;
+    public ParticleSystem thrusterEffect1;
+    public ParticleSystem thrusterEffect2;
 
     // Use this for initialization
     void Start()
@@ -19,37 +21,42 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 worldForcePosition;
         // Rotate based on object rotation.
-        Vector3 thrustForce = transform.rotation * Vector3.up * thrustStrength;
+        Vector3 thrustForce = transform.TransformDirection(Vector3.up) * thrustStrength;
 
-        if (Input.GetButton("Player1Thrust") && Input.GetButton("Player2Thrust"))
-        {
-            Vector3 newAngularVelocity = rigidbody.angularVelocity;
-            if (Mathf.Abs(rigidbody.angularVelocity.z) > velocityDecelerationWhenBothPress * Time.deltaTime) {
-                newAngularVelocity.z = rigidbody.angularVelocity.z + -Mathf.Sign(rigidbody.angularVelocity.z) * velocityDecelerationWhenBothPress * Time.deltaTime;
-            }
-            else {
-                newAngularVelocity.z = 0;
-            }
-            rigidbody.angularVelocity = newAngularVelocity;
-        }
+        bool p1Thrusting = Input.GetButton("Player1Thrust");
+        bool p2Thrusting = Input.GetButton("Player2Thrust");
 
-        if (Input.GetButton("Player1Thrust"))
-        {
-            worldForcePosition = transform.TransformPoint(Vector3.right * thrusterDistanceFromCenter);
-            rigidbody.AddForceAtPosition(thrustForce, worldForcePosition);
-
-        }
-        if (Input.GetButton("Player2Thrust"))
-        {
-            worldForcePosition = transform.TransformPoint(-Vector3.right * thrusterDistanceFromCenter);
-            rigidbody.AddForceAtPosition(thrustForce, worldForcePosition);
-        }
+        HandleThrusting(Vector3.right * thrusterDistanceFromCenter, thrustForce, p1Thrusting, thrusterEffect1);
+        HandleThrusting(-Vector3.right * thrusterDistanceFromCenter, thrustForce, p2Thrusting, thrusterEffect2);
 
         // Limit speed
         if (rigidbody.velocity.magnitude > maxSpeed)
         {
             rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
         }
-        Debug.Log(rigidbody.velocity.magnitude);
+    }
+
+    /// <summary>
+    /// Applies the thrust force and turns the thruster particle effect on and off.
+    /// </summary>
+    /// <param name="relativeThrustPosition">Thrust position relative to the ship center.</param>
+    /// <param name="thrustForce">Thrust force.</param>
+    /// <param name="thrustOn">If set to <c>true</c> apply thrust and keep particle effect playing. Otherwise no thrust and stop the 
+    ///             effect</param>
+    /// <param name="thrusterEffect">Thruster effect.</param>
+    void HandleThrusting(Vector3 relativeThrustPosition, Vector3 thrustForce, bool thrustOn, ParticleSystem thrusterEffect)
+    {
+        if (thrustOn)
+        {
+            Vector3 worldForcePosition = transform.TransformPoint(relativeThrustPosition);
+            rigidbody.AddForceAtPosition(thrustForce, worldForcePosition);
+            if (!thrusterEffect.isPlaying)
+                thrusterEffect.Play();
+        }
+        else
+        {
+            if (thrusterEffect.isPlaying)
+                thrusterEffect.Stop();
+        }
     }
 }
