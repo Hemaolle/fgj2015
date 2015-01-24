a9340b06b63007a5eb44c578322c778845385f49
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-01-24 18:48:11 +0200

    Added a Spin script.

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+    public float angularVelocity = 1;
+	
+    void Start() {
+        rigidbody.angularVelocity = new Vector3(0, 0, angularVelocity);
+    }
+}
