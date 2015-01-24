5f553d39eba03516e2a1da6aef3411b046319e95
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-01-24 13:24:55 +0200

    GameOver mechanics:
    -Enable game over text
    -Disable player movement
    -Reload level on pressing space

---
+++ Assets/Scripts/UI/GameOverScreen.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameOverScreen : MonoBehaviour {
+
+    public Health healthToMonitor;
+    public PlayerMovement playerMovement;
+    public GameObject [] enableOnGameOver;
+
+    private bool gameOver = false;
+
+	// Use this for initialization
+	void Start () {
+        healthToMonitor.healthZero += GameOver;
+	}
+	
+    void GameOver (HealthEvent e)
+    {
+        gameOver = true;
+        playerMovement.enabled = false;
+        foreach(GameObject enablee in enableOnGameOver) {
+            enablee.SetActive(true);
+        }
+    }
+
+    void Update() {
+
+        if (gameOver && Input.GetKey("space"))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
