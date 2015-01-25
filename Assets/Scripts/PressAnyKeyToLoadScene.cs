3f482f3a3f2cc596a98aec38aff373ac295e9368
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-01-25 14:34:45 +0200

    Added start menu and healthbar graphics (healthbar not in use yet)

---
+++ Assets/Scripts/PressAnyKeyToLoadScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressAnyKeyToLoadScene : MonoBehaviour {
+
+    public string sceneName;
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.anyKey)
+            Application.LoadLevel(sceneName);
+	}
+}
