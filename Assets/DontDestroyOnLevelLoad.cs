8a032382b60e4330fb4713e48ed48d196fa90dcf
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-01-25 15:27:38 +0200

    Fixed music, added License.

---
+++ Assets/DontDestroyOnLevelLoad.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontDestroyOnLevelLoad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (GameObject.FindGameObjectsWithTag("Music").Length > 1)
+            Destroy(gameObject);
+        GameObject.DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
